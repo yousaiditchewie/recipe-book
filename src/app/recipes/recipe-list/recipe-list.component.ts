57f5e86ac70066cca79449d82407dfbbbe30adc9
Jacob Allen <jedijake@me.com>
2017-03-10 00:14:35 -0800

    complete Ingredients List with *ngFor video

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -6,9 +6,12 @@ import { Recipe } from '../recipe';
   templateUrl: 'recipe-list.component.html'
 })
 export class RecipeListComponent implements OnInit {
-  recipes: Recipe[] = [];
+  recipes: Recipe[] = [
+    new Recipe('Schnitzel', 'Very tasty', 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ae/Wiener-Schnitzel02.jpg/220px-Wiener-Schnitzel02.jpg', []),
+    new Recipe('Summer Salad', 'Okayish', 'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/beetroot-feta-grain-salad.jpg', [])
+  ];
   @Output() recipeSelected = new EventEmitter<Recipe>();
-  recipe = new Recipe('Dummy', 'Dummy', 'http://www.digimadmedia.com/static/file-images/photo-images/47062607-Food-Table-Celebration-Delicious-Party-Meal-Concept-Stock-Photo.jpg');
+  // recipe = new Recipe('Dummy', 'Dummy', 'http://www.digimadmedia.com/static/file-images/photo-images/47062607-Food-Table-Celebration-Delicious-Party-Meal-Concept-Stock-Photo.jpg', []);
   constructor() { }
 
   ngOnInit() {
--- src/app/recipes/recipe.ts
+++ src/app/recipes/recipe.ts
@@ -1,5 +1,6 @@
+import { Ingredient } from '../../'
 export class Recipe {
-  constructor(public name: string, public description: string, public imagePath: string) {
+  constructor(public name: string, public description: string, public imagePath: string, public ingredients: Ingredient[]) {
     // name = this.name;
     // description = this.description;
     // imagePath = this.imagePath;
